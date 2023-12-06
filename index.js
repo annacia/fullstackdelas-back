d336b6e00a81a550e94f658344cc1be1521b743b
Anna Aureliano <anna.aureliano@picpay.com>
2023-12-05 22:16:05 -0300

    files

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require('dotenv').config();
+const firebaseAdmin = require("firebase-admin");
+const firebaseConfig = require("./config/firebaseConfig");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Inicializar o Firebase
+firebaseAdmin.initializeApp({
+  databaseURL: process.env.DATABASE_URL, 
+  credential: firebaseAdmin.credential.cert({
+    type: process.env.CREDENTIAL_TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_URL,
+    client_x509_cert_url: process.env.CLIENT_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN
+  }),
+});
+
+// Importar e usar as rotas
+const linkedinRoutes = require("./routes/linkedin");
+app.use("/linkedin", linkedinRoutes);
+
+// Outras rotas podem ser adicionadas da mesma forma
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
---
+++ routes/linkedin.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const router = express.Router();
+const firebaseAdmin = require("firebase-admin");
+
+// Obter uma referência ao banco de dados
+const db = firebaseAdmin.database();
+
+// Endpoint para criar um novo registro (OK)
+router.post("/", async (req, res) => {
+    const { nome, email, linkedin, frase } = req.body;
+    const ref = await db.ref("linkedin").push({ "nome": nome, "email": email, "linkedin": linkedin, "frase": frase }, function(error) {
+      if (error) {
+        console.log("Failed with error: " + error)
+        res.status(500).json({ error: "Erro interno do servidor" });
+      }
+    })
+    res.status(201).json({ "msg": "Registro cadastrado com sucesso", "key": ref.key });
+});
+  
+// Endpoint para obter todos os registros (OK)
+router.get("/", async (req, res) => {
+  db.ref("linkedin").once("value", (snapshot) => {
+    // O objeto 'snapshot' contém os dados do nó 'linkedin'
+    const linkedinData = snapshot.val();
+
+    const linkedinArray = Object.keys(linkedinData).map((key) => {
+      return {
+        id: key,
+        ...linkedinData[key]
+      };
+    });
+  
+    res.status(201).json(linkedinArray);
+  })
+  .catch((error) => {
+    console.error("Erro ao obter dados do Firebase:", error);
+    res.status(500).json({ error: "Erro interno do servidor" });
+  });
+});
+  
+  // Endpoint para obter um registro por ID (OK)
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    db.ref("linkedin").child(id).once("value", (snapshot) => {
+      const objetoData = snapshot.val();
+      if (objetoData) {
+        res.status(201).json(objetoData);
+      } else {
+        res.status(404).json({ error: "Registro não encontrado"});
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao obter dados do Firebase:", error);
+      res.status(500).json({ error: "Erro interno do servidor" });
+    });
+  } catch (error) {
+    console.error("Erro ao obter registro por ID:", error);
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
+});
+  
+// Endpoint para atualizar um registro por ID (OK)
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { nome, email, linkedin, frase } = req.body;
+  let objetoData = undefined
+
+  await db.ref("linkedin").child(id).once("value", (snapshot) => {
+    objetoData = snapshot.val();
+      if (!objetoData) {
+        res.status(404).json({ error: "Registro não encontrado"});
+      }
+  });
+
+  if (objetoData) {
+    await db.ref("linkedin").child(id).update({ 
+      "nome": nome,
+      "email": email,
+      "linkedin": linkedin,
+      "frase": frase
+    }).then(() => {
+      res.status(201).json({ "msg": "Registro atualizado com sucesso", "key": id });
+      console.log("Registro atualizado com sucesso!");
+    })
+    .catch((error) => {
+      console.error("Erro ao atualizar registro no Firebase:", error);
+      res.status(500).json({ error: "Erro interno do servidor" });
+    });
+  }
+});
+  
+// Endpoint para excluir um registro por ID (OK)
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await db.ref("linkedin").child(id).remove();
+    res.status(201).json({ message: "Registro excluído com sucesso" });
+  } catch (error) {
+    console.error("Erro ao excluir registro por ID:", error);
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
+});
+
+module.exports = router;
