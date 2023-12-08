99fb4b1e5f6f2e240a27be839af99fa231a3df2d
Anna Aureliano <anna.aureliano@picpay.com>
2023-12-07 21:06:57 -0300

    url swagger

--- swagger.js
+++ swagger.js
@@ -8,7 +8,7 @@ const doc = {
     },
     servers: [
         {
-            url: 'http://localhost:3000'
+            url: 'https://fullstackdelas-back.onrender.com/'
         }
     ],
     components: {
